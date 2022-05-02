c7d11a2d7bae4b8ee0d3c24886f933e62bc1ab25
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2022-05-02 10:14:43 +0200

    use new AbstractRiseClipseXMLLoadImpl

--- fr.centralesupelec.edf.riseclipse.iec61850.nsd/src/fr/centralesupelec/edf/riseclipse/iec61850/nsd/util/NsdLoadImpl.java
+++ fr.centralesupelec.edf.riseclipse.iec61850.nsd/src/fr/centralesupelec/edf/riseclipse/iec61850/nsd/util/NsdLoadImpl.java
@@ -1,6 +1,6 @@
 /*
 *************************************************************************
-**  Copyright (c) 2016-2021 CentraleSupélec & EDF.
+**  Copyright (c) 2016-2022 CentraleSupélec & EDF.
 **  All rights reserved. This program and the accompanying materials
 **  are made available under the terms of the Eclipse Public License v2.0
 **  which accompanies this distribution, and is available at
@@ -20,18 +20,12 @@
 */
 package fr.centralesupelec.edf.riseclipse.iec61850.nsd.util;
 
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.eclipse.emf.ecore.xmi.XMLHelper;
-import org.eclipse.emf.ecore.xmi.impl.XMLLoadImpl;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class NsdLoadImpl extends XMLLoadImpl {
+import fr.centralesupelec.edf.riseclipse.util.AbstractRiseClipseXMLLoadImpl;
+
+public class NsdLoadImpl extends AbstractRiseClipseXMLLoadImpl {
 
     public NsdLoadImpl( XMLHelper helper ) {
         super( helper );
@@ -41,24 +35,4 @@ public class NsdLoadImpl extends XMLLoadImpl {
         return new NsdXmlHandler( resource, helper, options );
     }
 
-    @Override
-    protected SAXParser makeParser() throws ParserConfigurationException, SAXException {
-        SAXParserFactory f = SAXParserFactory.newInstance();
-        f.setNamespaceAware( true );
-        return f.newSAXParser();
-    }
-
-    /*
-     * XMLLoadImpl.handleErrors() will throw either Resource.IOWrappedException or IOException
-     * for errors found while parsing document.
-     * Errors are not always fatal (like an InvalidValueException when an Integer
-     * is expected and something else is found).
-     * We will handle such errors later
-     * 
-     * TODO: this should be in riseclipse-main to be done for every file loaded by RiseClipse!
-     */
-    @Override
-    protected void handleErrors() throws IOException {
-        // Nothing
-    }
 }
